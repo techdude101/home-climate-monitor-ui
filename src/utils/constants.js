48dcc112f37a315ab95eec787c050ef29b5f67b3
techdude101 <techdude1011@gmail.com>
2021-12-08 19:33:41 +0000

    Changed API url from HTTP to HTTPS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // ## Temporary fix for Blocked loading mixed active content ##
-export const URL = 'https://cors-anywhere.herokuapp.com/http://ec2-54-74-135-132.eu-west-1.compute.amazonaws.com:5000';
+export const URL = 'https://ec2-54-74-135-132.eu-west-1.compute.amazonaws.com:5000';
